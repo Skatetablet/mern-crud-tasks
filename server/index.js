b6b8a509218593948942039bbdeb018d3cf49518
Skatetablet <skatewer@gmail.com>
2023-11-02 16:02:17 -0700

    Update server config

--- server/index.js
+++ server/index.js
@@ -17,8 +17,7 @@ app.use(taskRoutes)
 
 
 
-app.use(express.static(_dirname + '/dist'))
+app.use(express.static(_dirname + '../client/dist'))
 
-app.listen(process.env.PORT | PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+app.listen(PORT)
+console.log(`Server is running on port ${PORT}`);
