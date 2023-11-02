b9d5a489250c976c6ab1cd7203d03b134a192299
Skatetablet <skatewer@gmail.com>
2023-11-02 16:40:50 -0700

    Update

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,25 +1,25 @@
 import axios from "axios"
 
 export const getTasksRequest = async() => {
-    return await axios.get("http://localhost:4000/tasks")
+    return await axios.get("https://mern-tasks-crud.onrender.com/tasks")
 }
 
 export const createTaskRequest = async(task) => {
-    return await axios.post("http://localhost:4000/tasks", task)
+    return await axios.post("https://mern-tasks-crud.onrender.com/tasks", task)
 }
 
 export const deleteTaskRequest = async(id) => {
-    return await axios.delete(`http://localhost:4000/tasks/${id}`)
+    return await axios.delete(`https://mern-tasks-crud.onrender.com/tasks/${id}`)
 }
 
 export const getTaskRequest = async(id) => {
-    return await axios.get(`http://localhost:4000/${id}`)
+    return await axios.get(`https://mern-tasks-crud.onrender.com/${id}`)
 }
 
 export const updateTaskRequest = async(id, newFields) => {
-    return await axios.put(`http://localhost:4000/tasks/${id}`, newFields)
+    return await axios.put(`https://mern-tasks-crud.onrender.com/${id}`, newFields)
 }
 
 export const toggleTaskDoneRequest = async(id, done) => {
-    return await axios.put(`http://localhost:4000/tasks/${id}`, {done,})
+    return await axios.put(`https://mern-tasks-crud.onrender.com/${id}`, {done,})
 }
